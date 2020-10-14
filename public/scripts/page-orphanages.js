cc3d1769635311fa000dd1660f08c9333376ef6d
AnneFFlores <67755078+AnneFFlores@users.noreply.github.com>
2020-10-14 14:36:32 -0300

    page orphanage create

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,33 @@
+
+//create map
+const map = L.map('mapid').setView([-23.4270688,-51.9427964], 15)
+
+
+//create and add tileLayer
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+).addTo(map)
+
+
+// create icon
+const icon = L.icon({
+  iconUrl:"./public/images/map-marker.svg",
+  iconSize:[58, 68],
+  iconAnchor:[29,68],
+  popupAnchor:[170,2]
+})
+
+//create popup overlay
+
+const popup = L.popup({
+  closeButton:false,
+  className: 'map-popup',
+  minWidth:240,
+  minHeight:240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="chose-orphanage"> <img src="./public/images/arrow-white.svg"> class=""</a>')
+
+//create and add marker
+
+L.marker([-23.4270688,-51.9427964],{icon})
+.addTo(map)
+.bindPopup(popup)
