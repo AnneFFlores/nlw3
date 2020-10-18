4a569b835cfe0c7a70731da1169c8d6b9ee2160c
AnneFFlores <67755078+AnneFFlores@users.noreply.github.com>
2020-10-18 20:15:39 -0300

    create pages

--- src/database/test.js
+++ src/database/test.js
@@ -9,7 +9,8 @@ Database.then(async db => {
       lat: "-23.4270688",
       lng: "-51.9627964",
       name:"Lar de amor",
-      description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social." ,
+      about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social." ,
+      whatsapp: "4499967-0711",
       images: [        
         "https://images.unsplash.com/photo-1596908905631-7fe2dd220d24?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
       
@@ -30,4 +31,10 @@ Database.then(async db => {
  //consultar somente 1 orphanage, pelo ID
  const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
  console.log(orphanage)
+
+ //deletar dado da tabela 
+
+ console.log(await db.run("DELETE FROM orphanages WHERE id = '4'"))
+
+
 })
